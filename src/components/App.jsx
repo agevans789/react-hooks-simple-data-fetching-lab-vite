f939757bbd81714ae232795c4b4971bb049fbd12
Ann Grace Evans <agevans789@gmail.com>
2025-12-01 13:57:06 -0500

    3 tests passing

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -5,16 +5,22 @@ import { useState } from 'react';
 function App() {
     // load random dog from an API and display it
     const [dog, setDog] = useState(null);
+    const [loading, setLoading] = useState(true);
     const handleSetDog = async () => {
+        setLoading(true);
         fetch('https://dog.ceo/api/breeds/image/random')
             .then(response => response.json())
-            .then(data => setDog(data.message));
+            .then(data => {
+                setDog(data.message);
+                setLoading(false);
+            });
     };
     useEffect(() => {
         handleSetDog();
     }, []);
   return (
-    <div>
+    <div className='app'>
+        {loading ? <p>Loading...</p> : <img src={dog} alt="A Random Dog" />}
     </div>
   );
 }

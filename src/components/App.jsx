95c3b8fdecec04e6823bccb85475543b5f95592f
Ann Grace Evans <agevans789@gmail.com>
2025-12-01 13:53:31 -0500

    1 test passing

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,1 +1,22 @@
 // create your App component here
+import react, { useEffect } from 'react';
+import { useState } from 'react';
+
+function App() {
+    // load random dog from an API and display it
+    const [dog, setDog] = useState(null);
+    const handleSetDog = async () => {
+        fetch('https://dog.ceo/api/breeds/image/random')
+            .then(response => response.json())
+            .then(data => setDog(data.message));
+    };
+    useEffect(() => {
+        handleSetDog();
+    }, []);
+  return (
+    <div>
+    </div>
+  );
+}
+
+export default App;
